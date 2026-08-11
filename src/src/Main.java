8401855a2bc3b17bb95338566617f53595affd55
João Barreto <anothermailb@gmail.com>
2026-08-11 15:28:16 -0300

    criar branch barreto

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Isso não é um teste");
+        System.out.println("Essa é a branch de Barreto!");
     }
 }
