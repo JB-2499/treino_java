66093542f7b850a68fd00456379a9d75c8060b29
joao <email@email.com>
2026-08-14 15:55:39 -0300

    Cadastro de cleitinho

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Pessoa cleitinho = new Pessoa();
+        System.out.println(cleitinho.getIdade() + " " + cleitinho.getNome());
     }
 }
