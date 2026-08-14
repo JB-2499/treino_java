7cd8afd17e8cadced51fdb1f7a26066dc2a6ab57
joao <email@email.com>
2026-08-14 15:39:37 -0300

    Criar treino

---
+++ src/Pessoa.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public int funcao(int idade) {
+        idade += 1;
+        return idade;
+    }
+
+    public int funcao(int idade, String nome) {
+        idade += 2;
+        System.out.println(nome);
+        return idade;
+    }
+
+    public Pessoa() {
+        this.nome = "";
+        this.idade = 0;
+    }
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public static Pessoa criarPessoa(Scanner sc) {
+        Pessoa pessoa = new Pessoa();
+        System.out.println("Digite o nome:");
+        pessoa.nome = sc.nextLine();
+        System.out.println("Digite a idade:");
+        pessoa.idade = sc.nextInt();
+        sc.nextLine();
+        return pessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
--- src/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Isso não é um teste");
-        System.out.println("Essa é a branch de Barreto!");
-        System.out.println("Essa branch é de felipe lindo!!!");
-    }
-}
